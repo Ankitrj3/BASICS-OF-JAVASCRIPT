5cb1b58b607f054b72c5a11fc589923606545a50
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-05 18:53:10 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,12 @@
+function locallogic(val){
+    if(val<10){
+     console.log("the number is less than 10");
+    
+    }else if(val>5){
+        console.log("the number is more than 5");
+    }
+    else{
+        console.log("invalid number");
+    }
+}
+locallogic(23);
