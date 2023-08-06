614e269843fe01324b06684b85169a6ac468ad05
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-06 16:53:33 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,40 @@
+// we can declare the variables in 3 ways in js
+
+var a=12;
+let b="ankit";
+console.log(b);
+console.log(a);
+
+const anki="rj";
+console.log(anki);
+
+// data types 
+// primative data types
+// number,string,boolean,big int,undefined,null
+let k=12;
+let x="ankit";
+let o=null;
+const t=BigInt(22);
+var l=undefined;
+let d=true;
+
+console.log(k);
+console.log(x);
+console.log(o);
+console.log(t);
+console.log(l);
+console.log(d);
+
+// non primative data types
+
+// object
+
+let g={
+    ank:"ankit",
+    g:12,
+    r:13,
+    kalki:"pusp"
+
+};
+
+console.log(g);
