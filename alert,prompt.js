bbd123c7667dcdd6ca09b01c69dfd097ce6102e3
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-07 19:52:51 +0530

    Add files via upload

---
+++ alert,prompt.js
@@ -0,0 +1,7 @@
+let a = prompt("enter the value of a");
+let b = prompt("enter the value of b");
+if(a==b){
+    alert("your output is correct");
+}else if(a==2){
+    alert("it may be correct");
+}
