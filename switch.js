2608c55039227cd6a99466db94be1c968b28ae0f
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-08 16:39:26 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,25 @@
+let a = prompt ("enter the days in the form of integers\n");
+let daynumber=parseInt(a)
+switch(daynumber){
+    case 1:
+        alert('monday');
+        break;
+    case 2:
+        alert('tuesday');
+        break;
+    case 3:
+        alert('wednesday');
+        break;
+    case 4:
+        alert('thrusday');
+        break;
+    case 5:
+        alert('friday');
+        break;
+    case 6:
+        alert('saturday');
+        break;   
+    case 7:
+        alert('sunday');
+        break;  
+}
