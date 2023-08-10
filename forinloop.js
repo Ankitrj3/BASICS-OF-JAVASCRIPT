f3d09812709dac9ddf32a1833b0464af18e78c0a
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-10 14:55:19 +0530

    Add files via upload

---
+++ forinloop.js
@@ -0,0 +1,12 @@
+let a = {
+     1:"a",
+     2:"b",
+     3:"c",
+     4:"d"
+};
+
+for( i in a){
+    console.log("the loop :"+ i + "at:" +a[i]);
+};
+
+//we are printing the values in form of key and value
