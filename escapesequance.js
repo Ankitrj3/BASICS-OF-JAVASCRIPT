34b61684ee35fd93820b4da4568c702b742c729f
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-11 15:45:15 +0530

    Add files via upload

---
+++ escapesequance.js
@@ -0,0 +1,33 @@
+
+/*****
+ \' single quote
+ \" double quote
+ \\ backslash
+ \n new line
+ \r carriage return
+ \t tab
+ \b backspace
+ \f form feed
+ */
+
+
+ let a ="ank\'it";
+ console.log(a);
+
+ let b ="ank\"it";
+ console.log(b);
+
+ let c ="ank\\it";
+ console.log(c);
+
+ let d ="ank\nit";
+ console.log(d);
+
+ let e ="ank\bit";
+ console.log(e);
+
+ let f ="ank\tit";
+ console.log(f);
+
+ var string = "i am\"double quoted \"string inside";//if i want to show double quotes inside a string we need to add backward slash before the quotes
+console.log(string);
---
+++ stringinterpolation.js
@@ -0,0 +1,6 @@
+// string interpolation
+// we will use back tick
+let k="ankit";
+let k1="saurabh";
+let a = `${k} is a bestfriend of ${k1}`;
+console.log(a);
