da91465b3f939f4918e6604d348035780e8f5211
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-14 20:02:17 +0530

    Add files via upload

---
+++ prac1.js
@@ -0,0 +1,8 @@
+let text = "hello ankit how are you";
+let index = text.indexOf("are");
+
+let text1 = "Please locate where 'locate' occurs!";
+text1.search("locate");
+
+console.log(text);
+console.log(text1);
