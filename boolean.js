d1ad2c868e640db603b2c03f2cb31c27295f9427
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-15 19:55:26 +0530

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,14 @@
+let x = 0;
+Boolean(x);
+
+let x1 = -0;
+Boolean(x1);
+
+let x2;
+Boolean(x2);
+
+let x3 = false;
+let y = new Boolean(false);
+
+let x4 = new Boolean(false);
+let y4 = new Boolean(false)
